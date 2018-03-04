1eb2006b9098c23ce8c30b03fdd1d304365ae896
r3dark <sh.rohit485@gmail.com>
2018-03-04 13:33:08 +0530

    Update Holi And Colorful houses - hackerearth.cpp

--- Holi And Colorful houses - hackerearth.cpp
+++ Holi And Colorful houses - hackerearth.cpp
@@ -14,37 +14,37 @@ int main()
 	    while(q--)
 	    {
 	        int x, y, i, candy1 = 0, candy2 = 0;
-            cin >> x >> y;
-            i = --x;
-            y--;
-            while(i != y)
-            {
-                if(i == n - 1)
-                {
-                    candy1 += (s[i] != s[0]);
-                    i = 0;
-                }
-                else 
-                {
-                    candy1 += (s[i] != s[i + 1]);
-                    i++;
-                }
-            }
-            i = x;
-            while(i != y)
-            {
-                if(i == 0)
-                {
-                    candy2 += (s[i] != s[n - 1]);
-                    i = n - 1;
-                }
-                else 
-                {
-                    candy2 += (s[i] != s[i - 1]);
-                    i--;
-                }
-            }
-            (candy2 < candy1) ? cout << candy2 << endl : cout << candy1 << endl;
+	        cin >> x >> y;
+	        i = --x;
+	        y--;
+	        while(i != y)
+	        {
+	            if(i == n - 1)
+	            {
+	                candy1 += (s[i] != s[0]);
+	                i = 0;
+	            }
+	            else 
+	            {
+	                candy1 += (s[i] != s[i + 1]);
+	                i++;
+	            }
+	        }
+	        i = x;
+	        while(i != y)
+	        {
+	            if(i == 0)
+	            {
+	                candy2 += (s[i] != s[n - 1]);
+	                i = n - 1;
+	            }
+	            else 
+	            {
+	                candy2 += (s[i] != s[i - 1]);
+	                i--;
+	            }
+	        }
+	        (candy2 < candy1) ? cout << candy2 << endl : cout << candy1 << endl;
 	    }
 	}
 }
